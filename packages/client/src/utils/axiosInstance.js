3c642c3b48f55a8ac524cce538de03ee938f8b1a
Kelvin Lukorito <kelvin.lukorito@andela.com>
2019-08-02 04:39:44 +0300

    add express routes for react

--- packages/client/src/utils/axiosInstance.js
+++ packages/client/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://mysterious-falls-95987.herokuapp.com',
     timeout: 10000,
     headers: { 'Content-Type': 'application/json' },
 });
--- packages/server/app.js
+++ packages/server/app.js
@@ -3,6 +3,7 @@ const Unsplash = require('unsplash-js').default;
 const toJson = require('unsplash-js').toJson;
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 require('dotenv').config({ path: '../../.env' });
 
@@ -13,10 +14,19 @@ const unsplash = new Unsplash(config);
 const app = express();
 app.use(cors());
 
+app.use(express.static(path.join(__dirname, '../client/build')));
+
 app.get('/api/photos', (req, res) => {
     unsplash.photos.listPhotos(req.query.page, req.query.count)
         .then(toJson)
         .then(json => res.json(json))
+});
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
+
+app.all('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
